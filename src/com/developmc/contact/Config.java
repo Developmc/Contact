67cb806729303dd7edc2cbc210d85551132a4cdd
Developmc <Developmc@DaBianYLK>
2015-09-16 23:48:41 +0800

    update

--- src/com/developmc/contact/Config.java
+++ src/com/developmc/contact/Config.java
@@ -1,5 +1,31 @@
 package com.developmc.contact;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
 public class Config {
 
+	public static final String KEY_TOKEN = "token" ;
+	public static final String APP_ID = "com.developmc.contact" ;
+	
+	/**获取程序缓存token
+	 * @param context
+	 * @return
+	 */
+	public static String getCachedToken(Context context)
+	{
+		SharedPreferences preferences = context.getSharedPreferences(APP_ID, Context.MODE_PRIVATE);
+		return preferences.getString(KEY_TOKEN, null) ;
+	}
+	/**缓存token,写进sharedPreference
+	 * @param context
+	 * @param token
+	 */
+	public static void cachedToken(Context context,String token)
+	{
+		Editor editor = context.getSharedPreferences(APP_ID, Context.MODE_PRIVATE).edit() ;
+		editor.putString(KEY_TOKEN, token) ;
+		editor.commit() ;
+	}
 }
---
+++ src/com/developmc/contact/activity/TimelineActivity.java
@@ -0,0 +1,7 @@
+package com.developmc.contact.activity;
+
+import android.app.Activity;
+
+public class TimelineActivity extends Activity {
+
+}
